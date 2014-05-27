fe1385e739ac17c8cf4dce147321d3a9ef886222
Joseilton Costa Bruce <joseilton@n14add242853.14sprfpb>
2014-05-27 08:49:48 -0300

    Titulo home

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Assembleia de Deus - Bayeux - PB</title>
+<title>Assembleia de Deus - Alhandra - PB</title>
 
 <script type="text/javascript" src="js/funcoes.js"></script>
 <script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
