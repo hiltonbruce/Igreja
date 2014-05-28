2714fddc245528e67619decbc0ec29b4e7cf89c0
Joseilton Costa Bruce <joseilton@n14add242853.14sprfpb>
2014-05-28 16:22:15 -0300

    Backup mysqlDumper

--- func_class/constantes.php
+++ func_class/constantes.php
@@ -1,3 +1,8 @@
 <?PHP
         $dns = "mysql://igreja:G4Hd%VKC#yV5F!at8c@localhost/assembleia";
+        /*
+         * Usuário e senha de Backup:
+         * usuário: igrejaBKP
+         * senha: abPh!jUEyjs@8EK#xX4
+         */
 ?>
--- index.php
+++ index.php
@@ -89,6 +89,13 @@
           <li <?PHP id_left ("relatorio/");?>><a href="./?escolha=relatorio/formularios.php&menu=top_formulario"><span class="glyphicon glyphicon-list-alt" >&nbsp;</span>&nbsp;Recibos e Certid&otilde;es</a></li>
           <li <?PHP id_left ("aniv/");?>><a href="./?escolha=aniv/aniversario.php&menu=top_aniv"><span class="glyphicon glyphicon-gift" >&nbsp;</span>&nbsp;Aniversariantes</a></li>
           <li <?PHP id_left ("cetad");?>><a href="./?escolha=cetad/pgto.php&menu=top_cetad"><span class="glyphicon glyphicon-pencil" >&nbsp;</span>&nbsp;CETAD</a></li>
+          <?php 
+          	if ($_SESSION['nivel']>'10') {
+         	 ?>
+         		<li <?PHP id_left ("backup");?>><a href="./backup/"><span class="glyphicon glyphicon-export" >&nbsp;</span>&nbsp;Backup de Dados</a></li>
+		   	 <?php	
+          	}
+          ?>
 		  <?PHP
 		  }
 		  	else
