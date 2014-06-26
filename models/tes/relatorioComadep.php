893ce8ad96d1473ccb2d5b653c8fe687184d6b66
Joseilton Costa Bruce <joseilton@n14add242853.14sprfpb>
2014-06-26 09:29:04 -0300

    Relatorio COMADEP V.Beta

--- models/tes/relatorioComadep.php
+++ models/tes/relatorioComadep.php
@@ -5,7 +5,7 @@ $comSaldo	= '';$planoCta=array();$saldo=array();$cor=true;$sldGrupo=array();
 
 $plano = mysql_query('SELECT * FROM contas ORDER BY codigo ');
 while ($cta = mysql_fetch_array($plano)) {
-	$planoCta[$cta['id']]=array($cta['titulo'],$cta['acesso'],$cta['codigo'],$cta['tipo']);
+	$planoCta[$cta['id']]=array($cta['titulo'],$cta['acesso'],$cta['codigo'],$cta['tipo'],$cta['nivel4']);
 	$planoGrupo[$cta['codigo']]=array($cta['titulo'],$cta['codigo'],$cta['tipo']);
 }
 
@@ -16,14 +16,19 @@ $lista = mysql_query('SELECT  * FROM lancamento WHERE DATE_FORMAT(data,"%Y%m")="
 while ($contas = mysql_fetch_array($lista)) {
 	if ($contas['d_c']=='D') {
 		$saldo[$contas['conta']] += $contas['valor'];
+		$sldGrupo [$planoCta[$contas['conta']]['4']] += $contas['valor'];
 	}else {
 		$saldo[$contas['conta']] -= $contas['valor'];
+		$sldGrupo [$planoCta[$contas['conta']]['4']] -= $contas['valor'];
 	}
 	
-	$sldGrupo [$planoCta[$contas['conta']]['nivel4']] += $contas['valor'];
 }
 
 $ctaAtual = '';
+//print_r($sldGrupo);
+//print_r($saldo);
+
+//echo $planoCta['5']['4'];
 
 foreach ($saldo AS $chave => $valor){
 	
@@ -40,18 +45,44 @@ foreach ($saldo AS $chave => $valor){
 		
 		$vlrSaldo = number_format($vlrSaldo,2,',','.').$planoCta[$chave]['3'];
 		
-		if ($ctaAtual!=substr($planoCta[$chave]['2'],0,-4)){
+		//echo $planoCta[$chave]['4'].' -- ';
+		
+		if ($ctaAtual==$planoCta[$chave]['4'] || $ctaAtual==''){
+			//Contas simples
+			$nivel1 .='<tr '.$bgcolor.'><td>'.$planoCta[$chave]['2'].'</td><td>'.$acesso.'</td><td title="'.$title.'">'.$planoCta[$chave]['0'].
+			'</td><td id="moeda"></td><td id="moeda">'.$vlrSaldo.'</td></tr>';
+			$sldGrupoCta = abs($sldGrupo [$planoCta[$chave]['4']]);
+		}else {
 			//Grupo de contas
-			$ctaAtual = substr($planoCta[$chave]['2'],0,-4);
 			$bgcolorGrp = 'style="background:#B0C4DE; color:#000;border-bottom: 1px dashed #1e90ff;"';
-			$nivel2 .='<tr '.$bgcolorGrp.'><td>'.$planoGrupo[$ctaAtual]['1'].'</td><td></td><td title="'.$title.'">
-			'.$planoGrupo[$ctaAtual]['0'].'</td><td>'.number_format($sldConta,2,',','.').$planoGrupo[$ctaAtual]['2'].'</td><td id="moeda"></td></tr>';
+			$nivelGrupo ='<tr '.$bgcolorGrp.'><td>'.$planoGrupo[$ctaAtual]['1'].'</td><td></td><td title="'.$title.'">
+			'.$planoGrupo[$ctaAtual]['0'].'</td><td id="moeda">'.number_format($sldGrupoCta,2,',','.').$planoGrupo[$ctaAtual]['2'].'</td><td></td></tr>';
+			if ($nivel2=='') {
+				$nivel2 .=$nivelGrupo.$nivel1;
+			}else {
+				$nivelGrupo = $nivel2.$nivelGrupo.$nivel1;
+				$nivel2 = $nivelGrupo;
+				$nivelGrupo ='';
+			}
+			
+			$saldoAtual=0;
+			$sldGrupoCta = abs($sldGrupo [$planoCta[$chave]['4']]);
+			
+			if ($cor==false) {
+				$cor = !$cor;
+				$bgcolor = ($cor) ? 'style="background:#ffffff"' : 'style="background:#d0d0d0"';
+			}
+				
+			//Contas simples
+			$nivel1 ='<tr '.$bgcolor.'><td>'.$planoCta[$chave]['2'].'</td><td>'.$acesso.'</td><td title="'.$title.'">'.$planoCta[$chave]['0'].
+			'</td><td id="moeda"></td><td id="moeda">'.$vlrSaldo.'</td></tr>';
 		}
 		
-		$nivel2 .='<tr '.$bgcolor.'><td>'.$planoCta[$chave]['2'].'</td><td>'.$acesso.'</td><td title="'.$title.'">'.$planoCta[$chave]['0'].
-		'</td><td id="moeda"></td><td id="moeda">'.$vlrSaldo.'</td></tr>';
+			$cor = !$cor;
 		
-		$cor = !$cor;	
+		$ctaAtual = $planoCta[$chave]['4'];
+		
+			
 }
 
 //print_r($planoGrupo);
