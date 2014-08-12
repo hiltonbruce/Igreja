c5a11a8a7ac3b5958350813e10605280d9d67cb0
Joseilton Costa Bruce <joseilton@n14add242853.14sprfpb>
2014-08-12 17:05:24 -0300

    Relatorio Lancamento-Beta 0.2

--- models/tes/tes_relatorioLanc.class.php
+++ models/tes/tes_relatorioLanc.class.php
@@ -3,7 +3,7 @@ class tes_relatorioLanc {
 	
 function __construct() {
 		$this->var_string  = 'SELECT l.lancamento,DATE_FORMAT(l.data,"%d/%m/%Y") AS data,l.igreja,';
-		$this->var_string .= 'l.valor,l.hist,i.referente ';
+		$this->var_string .= 'l.valor,l.hist,i.referente,l.conta ';
 		$this->var_string .= 'FROM lancamento AS l, lanchist AS i ';
 		$this->var_string .= 'WHERE l.lancamento=i.idlanca';
 		
@@ -26,31 +26,32 @@ function histLancamentos ($igreja,$mes,$ano) {
 	$dquery = mysql_query($this->var_string.$opIgreja) or die (mysql_error());
 		
 	$tabela = '<tbody id="periodo">';
-	$histLancamento = '';
+	$histAtual = '';
+	
 	while ($linha = mysql_fetch_array($dquery)) {
-						
-		$valor = number_format($linha['valor'],2,',','.');
-			
-		$bgcolor = $cor ? '#d0d0d0' : '#ffffff';			
-			
+		$bgcolor = $cor ? '#d0d0d0' : '#ffffff';		
 		if ($congMembro!=$linha['congcadastro'] ) {
 			$congMembro = $linha['congcadastro'];
 			$dadosCongMembro = new DBRecord ('igreja',$linha['igreja'],'rol');
 			$nomeCongMembro = $dadosCongMembro->razao();
 		}
 		
-		$histLancamento=$histAtual;
 		
-		if ($histLancamento!='' && $histLancamento!=$histAtual) {
-			$referente = '<p>'.$histLancamento.'</p>';
+		
+		if ($histAtual!='' && $histAtual!=$histLancamento) {
+			$referente  = '<p>'.$titulo1.'</p>';
+			$referente .= '<p>'.$linha['conta'].'</p>';
+			$referente .= '<p>'.$histLancamento.'</p>';
 			$tabela .= '<tr style="background:'.$bgcolor.'"><td>'.$referente.'</td>
 			<td id="moeda">'.$valor.'</td></tr>';
+			$cor = !$cor;
 		}
-		
+		$histLancamento=$histAtual;
+		$titulo1 = $linha['conta'];
+		$valor = number_format($linha['valor'],2,',','.');
 		$histAtual = $linha['referente'];
 		
 		
-		$cor = !$cor;
 		}
 		
 	$resultado = array($tabela,$lancConfirmado);
