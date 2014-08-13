6460e1715ac5cf3c5746d4929618336976939916
Joseilton Costa Bruce <joseilton@n14add242853.14sprfpb>
2014-08-13 10:06:07 -0300

    Relatorio Lancamento-Beta 0.3

--- models/tes/tes_relatorioLanc.class.php
+++ models/tes/tes_relatorioLanc.class.php
@@ -3,7 +3,7 @@ class tes_relatorioLanc {
 	
 function __construct() {
 		$this->var_string  = 'SELECT l.lancamento,DATE_FORMAT(l.data,"%d/%m/%Y") AS data,l.igreja,';
-		$this->var_string .= 'l.valor,l.hist,i.referente,l.conta ';
+		$this->var_string .= 'l.valor,l.hist,i.referente,l.conta,l.d_c ';
 		$this->var_string .= 'FROM lancamento AS l, lanchist AS i ';
 		$this->var_string .= 'WHERE l.lancamento=i.idlanca';
 		
@@ -11,22 +11,22 @@ function __construct() {
 	
 function histLancamentos ($igreja,$mes,$ano) {
 	
-	$queryContas = mysql_query('SELECT id,acesso,titulo FROM contas') or die (mysql_error());
+	$queryContas = mysql_query('SELECT id,acesso,titulo,codigo FROM contas') or die (mysql_error());
 	while ($ctas = mysql_fetch_array($queryContas)) {
-		$conta [$cta['id']] = array ('acesso'=>$cta['acesso'],'titulo'=>$cta['titulo']);
+		$conta[$ctas['id']] = array ('acesso'=>$ctas['acesso'],'titulo'=>$ctas['titulo'],'codigo'=>$ctas['codigo']);
 	}
-	
+	//print_r ($conta);
 	if ($igreja=='0') {
 		$opIgreja = '';
 	}else {
 		$opIgreja= ' AND l.igreja = "'.$igreja;
 	}
 	
-	$opIgreja .= '" AND DATE_FORMAT(l.data,"%m%Y")="'.$mes.$ano.'" ORDER BY l.lancamento';
+	$opIgreja .= '" AND DATE_FORMAT(l.data,"%m%Y")="'.$mes.$ano.'" ORDER BY l.lancamento,l.id';
 	$dquery = mysql_query($this->var_string.$opIgreja) or die (mysql_error());
 		
 	$tabela = '<tbody id="periodo">';
-	$histAtual = '';
+	$lancAtual = '';  $lancamento = $lancAtual;
 	
 	while ($linha = mysql_fetch_array($dquery)) {
 		$bgcolor = $cor ? '#d0d0d0' : '#ffffff';		
@@ -37,19 +37,23 @@ function histLancamentos ($igreja,$mes,$ano) {
 		}
 		
 		
+		$lancAtual = $linha['lancamento'];
 		
-		if ($histAtual!='' && $histAtual!=$histLancamento) {
-			$referente  = '<p>'.$titulo1.'</p>';
-			$referente .= '<p>'.$linha['conta'].'</p>';
-			$referente .= '<p>'.$histLancamento.'</p>';
-			$tabela .= '<tr style="background:'.$bgcolor.'"><td>'.$referente.'</td>
-			<td id="moeda">'.$valor.'</td></tr>';
+		if ($lancAtual!=$lancamento && $lancamento!='') {
+			$dataLanc  = '<p>Data do Lan&ccedil;amento: '.$linha['data'].'</p>';
+			$referente .= $dataLanc.$titulo1;
+			$tabela .= '<tr style="background:'.$bgcolor.'"><td>'.$referente.$historico.'</td>
+			<td id="moeda">'.$lancValor.'</td></tr>';
 			$cor = !$cor;
+			$referente  = '';
+			$titulo1  = '';$lancValor = '';
 		}
-		$histLancamento=$histAtual;
-		$titulo1 = $linha['conta'];
+		
+		$historico  = '<p>Hist&oacute;rico: '.$linha['referente'].'</p>';
+		$lancamento = $lancAtual;
+		$titulo1  .= '<p>'.$conta[$linha['conta']]['codigo'].' - '.$conta[$linha['conta']]['titulo'].' - '.$linha['d_c'].'</p>';
 		$valor = number_format($linha['valor'],2,',','.');
-		$histAtual = $linha['referente'];
+		$lancValor .= '<p>'.$valor.' '.$linha['d_c'].'</p>';
 		
 		
 		}
