944c6e6bb2520824141a4182598b6fe48dc6a223
Joseilton <hiltonbruce@gmail.com>
2015-01-14 00:58:15 -0300

    Nova carta de recomendação, trasnf e mudança

--- relatorio/carta_print.php
+++ relatorio/carta_print.php
@@ -4,9 +4,10 @@
 	require_once ("../func_class/funcoes.php");
 	controle("consulta");
   	$carta 			= new DBRecord ("carta",$_POST["id_carta"],"id");
-	$membro 		= new DBRecord ("membro",$_SESSION["rol"],"rol");
-	$est_civil 		= new DBRecord ("est_civil",$_SESSION["rol"],"rol");
-	$ecles 			= new DBRecord ("eclesiastico",$_SESSION["rol"],"rol");
+	$membro 		= new DBRecord ("membro",$_POST['bsc_rol'],"rol");
+	$est_civil 		= new DBRecord ("est_civil",$_POST['bsc_rol'],"rol");
+	$ecles 			= new DBRecord ("eclesiastico",$_POST['bsc_rol'],"rol");
+	$profissional	= new DBRecord ("profissional",$_POST['bsc_rol'],"rol");
 	$igreja 		= new DBRecord ("igreja","1","rol");
 	$cid_batismo 	= new DBRecord ("cidade",$ecles->local_batismo(),"id");
     $rol 			= $_POST["bsc_rol"];
@@ -24,16 +25,26 @@
 <title>Carta de <?PHP echo carta($carta->tipo());?></title>
 <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
 <link rel="stylesheet" type="text/css" href="style.css" />
-<link rel="icon" type="image/gif" href="../img/br_igreja.gif">
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="../css/docs.min.css" />
+<link rel="icon" type="image/gif" href="../ad.ico">
 </head>
 <body>
 <div id="container">
-  <div id="header"></div>
+  <div id="header"  class='row'>
+  <div class="col-md-2"></div>
+  <div class="col-md-9 text-left">
+	<?PHP echo "Templo SEDE: {$igreja->rua()}, N&ordm; {$igreja->numero()} - $origem - {$igreja->uf()}";?><br />
+	  <?PHP echo " CNPJ: {$igreja->cnpj()} - CEP: {$igreja->cep()} - Fone: {$igreja->fone()} <br />";?>
+	  Copyright &copy; http://<?PHP echo "{$igreja->site()}";?>/
+      Email: <?PHP echo "{$igreja->email()}";?> 
+   	</div>
+  </div>
 <div id="mainnav">
     <div id="foto">
   	<?PHP print mostra_foto($rol);?></div>
 	<div id="Tipo">
-	  <h3>
+	  <h3><u><strong>
 	Carta <?PHP //Tipo de carta - Recomendação ou Mudança
 	print carta ("{$carta->tipo()}");
 	
@@ -47,29 +58,69 @@
 		}
 	
 	if ($carta->tipo()!=="3") {
-		$intr = "a {$carta->igreja()}, em $destino,";
+		$intr .= '<div class="panel panel-default text-center">';
+		$intr .= '	 <div class="panel-body">';
+		$intr .= '	    DESTINO: '.$carta->igreja().', em '.$destino;
+		$intr .= '	</div>';
+		$intr .= '</div>';
+
 	}else {
 		$intr = "";
 	}
-  ?></h3>
+  ?></strong></u></h3>
   </div>
   </div>
 	<div id="content">
     <div id="added-div1">
-      <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Apresentamos <?PHP echo $intr; if ($membro->sexo()=="M"){ print " o irm&atilde;o ";} else {print " a irm&atilde; ";} print strtoupper( toUpper($membro->nome())).", ".$est_civil->estado_civil();?>,   <?PHP print cargo($_SESSION["rol"]); ?> da Igreja desde <?PHP print conv_valor_br((cargo_dt ())); ?>, com cadastro no Rol de membros sob o n&ordm; <?PHP printf ("%'03u",$_SESSION["rol"]); ?>, teve seu  batismo em &aacute;guas na cidade de <?PHP print "{$cid_batismo->nome()} - {$cid_batismo->coduf()} no dia ".conv_valor_br($ecles->batismo_em_aguas());?>. E  serve ao Senhor nesta igreja, e por se achar em comunh&atilde;o, &eacute; que  recomendamos para que <?PHP if ($membro->sexo()=="M"){ print "o";} else {print "a";} ?> recebais no Senhor como ousam fazer os Santos.</p>
+			    <?PHP echo $intr;?>
+    	<p>Sauda&ccedil;&otilde;es no Senhor:</p>
+
+     	<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Apresentamos a amada Igreja, <?PHP 
+      	if ($membro->sexo()=="M"){ 
+      			print "<strong> o irm&atilde;o: </strong></p>";} 
+      		else {
+      			print "<strong> a irm&atilde; </strong></p>";} ?>
+    <table class='table table-bordered'>
+		<tbody>
+			<tr>
+				<td><h4><small>Rol</small><br><?php printf ("%'03u",$_POST['bsc_rol']); ?></h4></td>
+				<td colspan='3'><h4><small>Nome:</small><br><?php print strtoupper( toUpper($membro->nome())); ?></h4></td>
+				<td><h4><small>Func. Eclesiast&iacute;ca:</small><br><?php print cargo($_POST['bsc_rol']); ?></h4></td>
+				<td><h4><small>Est. Civil</small><br><?php print $est_civil->estado_civil(); ?></h4></td>
+			</tr>
+			<tr>
+				<td colspan='2'><h4><small>RG:</small><br><?php print $profissional->rg(); ?></h4></td>
+				<td><h4><small>CPF:</small><br><?php print $profissional->cpf(); ?></h4></td>
+				<td><h4><small>Data de Nasc.:</small><br><?php print conv_valor_br($membro->datanasc()); ?></h4></td>
+				<td><h4><small>Batismo</small><br><?php print conv_valor_br($ecles->batismo_em_aguas()); ?></h4></td>
+				<td><h4><small>Membro Desde:</small><br><?php print conv_valor_br($ecles->dat_aclam()); ?></h4></td>
+			</tr>
+		</tbody>
+	</table>
+      	<p class='text-justify'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;E  serve ao Senhor nesta igreja, e por se achar em comunh&atilde;o, &eacute; que  recomendamos para que <?PHP 
+      	if ($membro->sexo()=="M"){
+      	 print "o";} 
+      	 else {
+      	 	print "a";} ?> recebais no Senhor como usam fazer os Santos.
+      	 <p>
       <fieldset>
 	  <legend>Observa&ccedil;&otilde;es:</legend>
       <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?PHP echo $carta->obs(); ?>.</p>
 	  </fieldset>
     </div>
-    <div id="added-div-2">
-      <h3><?PHP  print $origem." - ".$igreja->uf().", ".data_extenso (conv_valor_br($carta->data()));?></h3>
+    <div class='text-right'>
+      <?PHP  print $origem." - ".$igreja->uf().", ".data_extenso (conv_valor_br($carta->data()));?>
       <p>&nbsp;</p>
-      <p class="bottom">&nbsp;</p>
+      <p>&nbsp;</p>
 	  <div id="pastor"><?PHP echo strtoupper(toUpper($igreja->pastor()));?><br />
 	    Pastor da Igreja</div>
 	  <div id="secretario"><?PHP echo strtoupper($_POST["secretario"]);?><br />
       Secret&aacute;rio </div>
+	    <div class="bs-callout bs-callout-info text-center" id="callout-helper-context-color-accessibility">
+	    	<h4>Alcan&ccedil;ando Vidas para Cristo</h4>
+		</div>
+
+
 	  <div id="vencimento">Esta carta deve ser apresentada a igreja destinat&aacute;ria at&eacute;:
         <?PHP 
 		echo data_venc(conv_valor_br($carta->data()));
@@ -77,12 +128,6 @@
 	  </div>
     </div>
     <div id="footer">
-	<?PHP echo "Templo SEDE: {$igreja->rua()}, N&ordm; {$igreja->numero()} - $origem - {$igreja->uf()}";?><br />
-
-	  Copyright &copy; <a href="http://<?PHP echo "{$igreja->site()}";?>/" title="Copyright information"></a>
-      Email: <a rel="nofollow" target="_blank" href="mailton: <?PHP echo "{$igreja->email()}";?>"><?PHP echo "{$igreja->email()}";?></a> <br />
-	   <?PHP echo "CNPJ: {$igreja->cnpj()}";?><br />
-   		<?PHP echo "CEP: {$igreja->cep()} - Fone: {$igreja->fone()} - Fax: {$igreja->fax()}";?><br />
 	  <p>Designed by <a rel="nofollow" target="_blank" href="mailton: hiltonbruce@gmail.com">Joseilton Costa Bruce.</a></p>
     </div>
   </div>
