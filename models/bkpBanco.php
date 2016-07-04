9ad718f8367a4f2b842983884ff269a4711a0ca9
Joseilton C Bruce <hiltonbruce@gmail.com>
2016-07-04 16:47:23 -0300

    Bakcup versão Beta

--- models/bkpBanco.php
+++ models/bkpBanco.php
@@ -16,4 +16,20 @@ sleep(10);
    }
    $diretorio -> close();
 */
+sleep(10);
+
+// variável que define o diretório das imagens
+$dir = "./bkpbanco/";
+
+// esse seria o "handler" do diretório
+$dh = opendir($dir);
+
+// loop que busca todos os arquivos até que não encontre mais nada
+while (false !== ($filename = readdir($dh))) {
+// verificando se o arquivo é .jpg
+	if (substr($filename,-4) == ".sql") {
+	// mostra o nome do arquivo e um link para ele - pode ser mudado para mostrar diretamente a imagem :)
+	echo '<a href='.$dir.'\\'.$filename.'><img src="img/bkp.ico" width="24px" height="24px"/> '.$filename.'</a><br>';
+	}
+}
 ?>
