7c9262325d1cf75d6411be09eaf37ec7e9768773
Joseilton C Bruce <hiltonbruce@gmail.com>
2016-08-28 09:04:53 -0300

    Ajuste BD Backup

--- views/backup.php
+++ views/backup.php
@@ -9,7 +9,7 @@ if (!empty($_GET['gerar'])) {
 		// loop que busca todos os arquivos até que não encontre mais nada
 		$arq = array();
 		while (false !== ($filename = readdir($dh))) {
-		// verificando se o arquivo é .sql
+		// verificando se o arquivo é .jpg
 			if (substr($filename,-4) == ".jpg") {
 				// mostra o nome do arquivo e um link para ele - pode ser mudado para mostrar diretamente a imagem :)
 				$arq[] = $filename;
