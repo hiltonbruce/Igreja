c44b2b925cfdba9e8376225204337d9d07a65550
Joseilton C Bruce <hiltonbruce@gmail.com>
2016-08-30 22:43:15 -0300

    Tela de login V2.01

--- views/login.php
+++ views/login.php
@@ -13,22 +13,20 @@ class=""></li> <li class="active" data-target="#carousel-example-captions" data-
  <div class="carousel-caption">
  </div> </div>
  <div class="item active">
-<img data-holder-rendered="true" src="img/debian.png" alt="900x500" width="350" height="600" data-src="holder.js/900x500/auto/#666:#666">
+<img data-holder-rendered="true" src="img/debian.png" alt="900x500" width="333" height="600" data-src="holder.js/900x500/auto/#666:#666">
 <div class="carousel-caption">
 <h3>Joseilton Costa Bruce</h3>
 <p>hiltonbruce@gmail.com</p>
 </div> </div>
 <div class="item">
-<img data-holder-rendered="true" src="img/lamp.png" alt="900x500" width="500" data-src="holder.js/900x500/auto/#555:#555">
+<img data-holder-rendered="true" src="img/lamp.png" alt="900x500" width="416" >
 <div class="carousel-caption">
 <h3></h3>
 <p></p>
 </div> </div> </div>
 <a href="#carousel-example-captions" class="left carousel-control" role="button" data-slide="prev">
-<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 <span class="sr-only">Previous</span> </a>
 <a href="#carousel-example-captions" class="right carousel-control" role="button" data-slide="next">
-<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 <span class="sr-only">Next</span> </a>
 </div></div>
 
