b78e187603fe6442151119e1f928dc73cf66c179
Joseilton C Bruce <hiltonbruce@gmail.com>
2018-10-29 16:48:45 -0300

    Bug Semana

--- func_class/funcoes.php
+++ func_class/funcoes.php
@@ -371,23 +371,20 @@ function semana ($data) {
 	if (checkdate($m,$d,$y)) {
 		//echo '<h1>'.$d.'/'.$m.'<br/>'.date('w',mktime(1,0,0,$m,$i,$y)).'</h1>';
 		//echo '<h2>'.$semana.'</h2>';
-		
-            
+
+
 		for ($i = 1; $i <= $diafim; $i++) {//Verifica a q semana pertence o lan�amento
 			//echo $d.' ++++++++++ '.$i;
-			
-			if ($d==$i) {
-            return $semana;
-            }
-            
-			if (date('N',mktime(1,0,0,$m,$i,$y))=='6' && $semana<5) {
+
+			if (date('N',mktime(1,0,0,$m,$i,$y))=='1' && $semana<5 && date('N',mktime(1,0,0,$m,0,$y))!='0') {
+
 				$semana++;
 				//echo '<h2>'.$semana.'</h2>';
 			}
 			//echo(date('d/M/Y',mktime(1,0,0,$m,$i,$y)).' <--> '.date('w',mktime(1,0,0,$m,$i,y)).' ********** ');
 		}
 	}
-	
+
 }
 
 function diaSem ($data) {
