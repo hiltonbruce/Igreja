a0b881eff520955241deb86ffb9fbd5cb5da2055
Joseilton C Bruce <hiltonbruce@gmail.com>
2018-11-12 16:07:51 -0300

    Certificado de Batismo - Alfa01

--- views/secretaria/batismoCertificado.php
+++ views/secretaria/batismoCertificado.php
@@ -10,23 +10,30 @@ $image = imagecreatefrompng('../../img/354.png');
 
 $certifico = 'Certificamos que: ';
 $sexo = ($_POST['sexo']=='M' || $_POST['sexo']=='m') ? 'batizado' : 'batizada' ;
-$certCorpo = 'foi '.$sexo.' nas águas em '.$d.'/'.$m.'/'.$y.', em nome do Pai, do Filho ';
-$certCorpo .= 'e do Esp&iacute;rito';
 
 $titleColor = imagecolorallocate($image, 0, 0, 0);
 $certifColor = imagecolorallocate($image, 255, 255, 0);
 $batismoColor = imagecolorallocate($image, 255, 255, 255);
 $grey = imagecolorallocate($image, 100, 100, 100);
 
-imagettftext($image, 45, 0, 120, 150, $certifColor, '../../fonts/Catamaran/Catamaran-Bold.ttf','CERTIFICADO');
-imagettftext($image, 25, 0, 120, 190, $batismoColor, '../../fonts/Catamaran/Catamaran-Bold.ttf','DE BATISMO');
-imagettftext($image, 32, 0, 440, 450, $titleColor, '../../fonts/Pinyon_Script/PinyonScript-Regular.ttf', $nome);
+imagettftext($image, 40, 0, 770, 80, $certifColor, '../../fonts/Catamaran/Catamaran-Bold.ttf','CERTIFICADO');
+imagettftext($image, 20, 0, 940, 110, $batismoColor, '../../fonts/Catamaran/Catamaran-Bold.ttf','DE BATISMO');
+imagettftext($image, 32, 0, 410, 410, $titleColor, '../../fonts/Pinyon_Script/PinyonScript-Regular.ttf', $nome);
 
-imagettftext($image, 14, 0, 500, 380, $titleColor, '../../fonts/Playfair_Display/PlayfairDisplay-Regular.ttf',$certifico);
-imagettftext($image, 14, 0, 430, 520, $titleColor, '../../fonts/Playfair_Display/PlayfairDisplay-Regular.ttf',$certCorpo);
+imagettftext($image, 14, 0, 500, 340, $titleColor, '../../fonts/Playfair_Display/PlayfairDisplay-Regular.ttf',$certifico);
 
-$certCorpo = 'Santo, conforme mandamento do Senhor Jesus em mateus 28.19';
-imagettftext($image, 14, 0, 430, 560, $titleColor, '../../fonts/Playfair_Display/PlayfairDisplay-Regular.ttf',$certCorpo);
+$certCorpo = 'foi '.$sexo.' nas águas em '.$d.'/'.$m.'/'.$y.', conforme  mandamento do Senhor Jesus';
+imagettftext($image, 14, 0, 400, 480, $titleColor, '../../fonts/Playfair_Display/PlayfairDisplay-Regular.ttf',$certCorpo);
+
+$certCorpo = 'em Mateus 28.19 ';
+imagettftext($image, 14, 0, 400, 520, $titleColor, '../../fonts/Playfair_Display/PlayfairDisplay-Regular.ttf',$certCorpo);
+
+$certCorpo = '("Portanto, ide, ensinai todas as nações, batizando-as em nome';
+imagettftext($image, 14, 0, 540, 520, $titleColor, '../../fonts/Playfair_Display/PlayfairDisplay-Italic.ttf',$certCorpo);
+$certCorpo = 'do Pai, e  do Filho, e do Espírito Santo"). ARC';
+imagettftext($image, 14, 0, 400, 560, $titleColor, '../../fonts/Playfair_Display/PlayfairDisplay-Italic.ttf',$certCorpo);
+$certCorpo = '';
+imagettftext($image, 14, 0, 400, 600, $titleColor, '../../fonts/Playfair_Display/PlayfairDisplay-Regular.ttf',$certCorpo);
 
 imagestring($image, 3, 750, 630, 'Emissão: '.data_extenso (date('d/m/Y')), $titleColor);
 imagestring($image, 3, 420, 714, $secretario, $titleColor);
