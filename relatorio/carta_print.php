fd765c602e0af196bcaad2ec31b0bb9c09757d57
Joseilton C BRuce <bruce@HiltonDebian.14SRPRFPB.PRF.GOV.BR>
2020-02-03 13:33:44 -0300

    Ocultar erros

--- relatorio/carta_print.php
+++ relatorio/carta_print.php
@@ -1,6 +1,7 @@
 <?PHP
 	session_start();
-	error_reporting(E_ALL);
+	error_reporting(0);
+	ini_set(“display_errors”, 0 );
   require "../func_class/funcoes.php";
   require "../func_class/classes.php";
   function __autoload ($classe) {
